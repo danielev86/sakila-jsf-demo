8b9899eb7775c77d28244cc716a61ff3ac70b328
danieleverrienti86 <danieleverr@gmail.com>
2022-07-17 08:02:38 +0100

    SJD1 - Create customer microservice

---
+++ sakila-customer-microservice/src/main/java/com/danielev86/customermicro/SakilaCustomerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.danielev86.customermicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaCustomerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaCustomerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ sakila-customer-microservice/src/test/java/com/danielev86/customermicro/SakilaCustomerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielev86.customermicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaCustomerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
